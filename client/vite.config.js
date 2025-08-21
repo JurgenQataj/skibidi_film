381d300260172f9afe0ed3528ca82e3fc37b626e
Giuggiola69 <jurgenklopp144@gmail.com>
2025-08-21 17:52:35 +0200

    Stile: Aggiunta responsività mobila

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // Rende il server visibile sulla tua rete locale (Wi-Fi)
-    host: true,
-    proxy: {
-      // Qualsiasi richiesta che il frontend fa a "/api/..."
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      host: true,
+    },
+  };
+
+  if (command === "serve") {
+    config.server.proxy = {
       "/api": {
-        // ...verrà reindirizzata al tuo server backend
         target: "http://localhost:5000",
-        // Necessario per evitare problemi di CORS
         changeOrigin: true,
       },
-    },
-  },
+    };
+  }
+
+  return config;
 });
