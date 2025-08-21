cc60ecd5bdee3a20ecdf41cb887eff1efe8982bd
Giuggiola69 <jurgenklopp144@gmail.com>
2025-08-21 18:24:34 +0200

    Stile: Aggiunta responsività mobila

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
@@ -10,7 +9,7 @@ export default defineConfig(({ command }) => {
     },
   };
 
-  // Applica il proxy solo quando esegui `npm run dev`
+  // Applica il proxy solo quando esegui `npm run dev` in locale
   if (command === "serve") {
     config.server.proxy = {
       "/api": {
