d540d6db4303c7bacd77dc7283d43bf63d2ca561
Giuggiola69 <jurgenklopp144@gmail.com>
2025-09-24 10:42:42 +0200

    Stile: Aggiunta responsività mobila

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
-// IMPORTANTISSIMO: Tutte le route specifiche PRIMA di /:tmdbId
+// ❗ IMPORTANTE: TUTTI gli endpoint specifici DEVONO venire PRIMA di /:tmdbId
+
+// Endpoint di test
 router.get("/test", (req, res) => {
   res.json({
     message: "Endpoint test funziona!",
@@ -11,12 +13,20 @@ router.get("/test", (req, res) => {
   });
 });
 
+// Endpoint per suggestions
 router.get("/suggestions", movieController.getMovieSuggestions);
+
+// Endpoint per search
 router.get("/search", movieController.searchMovies);
+
+// Endpoint per trending
 router.get("/trending/week", movieController.getTrendingMovies);
+
+// Endpoint per top-rated
 router.get("/top-rated/internal", movieController.getTopRatedMovies);
 
-// QUESTA DEVE ESSERE SEMPRE L'ULTIMA!
+// ❗ QUESTA ROUTE DEVE ESSERE SEMPRE L'ULTIMA!
+// Perché /:tmdbId cattura QUALSIASI cosa che non sia stata matchata prima
 router.get("/:tmdbId", movieController.getMovieDetails);
 
 module.exports = router;
