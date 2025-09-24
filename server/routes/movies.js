9cdc100da68db257a25b506696e2e29687bb7b68
Giuggiola69 <jurgenklopp144@gmail.com>
2025-09-24 10:35:41 +0200

    Stile: Aggiunta responsività mobila

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
-// IMPORTANTE: suggestions DEVE essere prima di search e :tmdbId
+// ENDPOINT DI TEST per debug
+router.get("/test", (req, res) => {
+  res.json({
+    message: "Route funziona!",
+    hasApiKey: !!process.env.TMDB_API_KEY,
+    query: req.query,
+  });
+});
+
+// Route suggestions
 router.get("/suggestions", movieController.getMovieSuggestions);
 
-// Route esistenti (invariate)
+// Altre route esistenti
 router.get("/search", movieController.searchMovies);
 router.get("/trending/week", movieController.getTrendingMovies);
 router.get("/top-rated/internal", movieController.getTopRatedMovies);
