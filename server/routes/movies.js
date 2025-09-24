7b05a1e48b6c202f5c4033ff96e548b3a1671c27
Giuggiola69 <jurgenklopp144@gmail.com>
2025-09-24 10:26:25 +0200

    Stile: Aggiunta responsività mobila

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,19 +2,13 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
-// Route per ricerca con suggerimenti
+// IMPORTANTE: suggestions DEVE essere prima di search e :tmdbId
 router.get("/suggestions", movieController.getMovieSuggestions);
 
-// Route per ricerca completa
+// Route esistenti (invariate)
 router.get("/search", movieController.searchMovies);
-
-// Route per dettagli film
-router.get("/:tmdbId", movieController.getMovieDetails);
-
-// Route per film di tendenza
 router.get("/trending/week", movieController.getTrendingMovies);
-
-// Route per film top rated
 router.get("/top-rated/internal", movieController.getTopRatedMovies);
+router.get("/:tmdbId", movieController.getMovieDetails);
 
 module.exports = router;
