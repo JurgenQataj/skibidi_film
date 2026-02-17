dd42c06e877bcef5741d8b8140b62a6036003fb2
JuriQ <jurgenklopp144@gmail.com>
2026-02-17 19:14:14 +0100

    pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,55 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          name: 'Skibidi Film',
+          short_name: 'Skibidi Film',
+          description: 'Track your watched movies and reviews',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
     server: {
       host: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+        },
+      },
+    },
+    preview: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+        },
+      },
     },
   };
-
-  // Applica il proxy solo quando esegui `npm run dev` in locale
-  if (command === "serve") {
-    config.server.proxy = {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    };
-  }
 
   return config;
 });
