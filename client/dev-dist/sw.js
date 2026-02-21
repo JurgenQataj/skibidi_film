33885cf66dacae587c9928c89b720471f0742a42
JuriQ <jurgenklopp144@gmail.com>
2026-02-21 18:21:29 +0100

    design

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5uk9b4cd82g"
+    "revision": "0.vg66v8m6kcc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
