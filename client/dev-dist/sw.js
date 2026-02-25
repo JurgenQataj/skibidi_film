3f6b8c8c1b11ef2a8fe661e3da5d66c0f85b1674
JuriQ <jurgenklopp144@gmail.com>
2026-02-25 19:07:45 +0100

    forum

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8c95fiq427g"
+    "revision": "0.l92qs739ngc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
