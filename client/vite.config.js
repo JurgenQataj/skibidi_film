07056b181717dc35f5da1532bb99f786cd164401
JuriQ <jurgenklopp144@gmail.com>
2026-03-09 20:35:24 +0100

    forum

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ command }) => {
     plugins: [
       react(),
       VitePWA({
-        registerType: 'autoUpdate',
+        registerType: 'prompt',
         includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
         devOptions: {
           enabled: true
